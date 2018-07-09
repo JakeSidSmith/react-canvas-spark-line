5d7b1d36e381c4faae4a2e0bff0828a8b959fb99
Jake 'Sid' Smith <jake@dabapps.com>
2018-07-09 13:52:03 +0100

    Additioal examples

--- examples/src/js/index.js
+++ examples/src/js/index.js
@@ -38,14 +38,52 @@ class App extends React.Component {
           width={100}
           height={30}
           color="blue"
+          data={[0]}
+        />
+
+        <SparkLine
+          animate
+          width={100}
+          height={30}
+          color="red"
           data={[10, 10]}
         />
 
         <SparkLine
           width={100}
           height={30}
-          color="red"
+          color="green"
           data={[1, 2, 1]}
+        />
+
+        <SparkLine
+          width={100}
+          height={30}
+          color="blue"
+          data={[1, 2, 1]}
+          includeZero={false}
+        />
+
+        <SparkLine
+          width={100}
+          height={30}
+          color="red"
+          data={[-10, 20, 5]}
+        />
+
+        <SparkLine
+          width={100}
+          height={30}
+          color="green"
+          data={[-10, -20, -5]}
+        />
+
+        <SparkLine
+          width={100}
+          height={30}
+          color="blue"
+          data={[-10, -20, -5]}
+          includeZero={false}
         />
       </div>
     );
